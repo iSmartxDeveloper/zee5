9b1eca2332e9b4ab412f0b29545839a4003b7e31
ꜱᴍᴀʀᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ <106044904+iSmartxDeveloper@users.noreply.github.com>
2022-11-21 16:21:31 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,12 +3,12 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5772038344:AAEcIiMZQOpx4h17sf3KLJ16KUyXXQmCpj4")
 
     # The Telegram API things
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", "15532790"))
+    API_HASH = os.environ.get("API_HASH", "52c9ce174794ba4c750f71978c8dfa65")
 
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
@@ -29,5 +29,5 @@ class Config(object):
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
     
     # Sql Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://publicdatabase:2EzVBS74xlPzXkTn@cluster0.0ujkh.mongodb.net/?retryWrites=true&w=majority")
     
